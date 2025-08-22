e8248b09f3d1c761890a5587f8b36a3594a8ceaa
Jakia123sultana <jakiasultanaania@gmail.com>
2025-08-22 17:22:51 +0600

    change in loginpage

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -110,7 +110,7 @@ export default function LoginPage() {
         <h1 className="text-2xl font-bold mb-8 text-center text-purple-600">Login</h1>
 
         {/* Email/Password form */}
-        <form onSubmit={handleEmailLogin} className="mb-6">
+        {/* <form onSubmit={handleEmailLogin} className="mb-6">
           <div className="mb-4">
             <label className="block text-gray-700 mb-2">Email</label>
             <input
@@ -142,8 +142,8 @@ export default function LoginPage() {
           >
             {loading ? "Logging in..." : "Login"}
           </button>
-        </form>
-
+        </form> */}
+   <h1>This is nice to adede</h1>
         <div className="text-center text-gray-400 mb-4">OR</div>
 
         {/* Google login */}
@@ -156,6 +156,7 @@ export default function LoginPage() {
         >
           {loading ? "Logging in..." : "Login with Google"}
         </button>
+        
       </div>
     </div>
   );
